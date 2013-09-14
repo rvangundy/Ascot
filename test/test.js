d9db7b71fe4307ee2ec96d1f403d7b7191200fd3
rvangundy <ryanvangundy@Todds-MacBook-Pro.local>
2013-09-13 21:47:40 -0400

    Latest ascot tested

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-/* global test, ok, equal, deepEqual, Ascot */
+/* global test, ok, equal, deepEqual, ascot */
 'use strict';
 
 // PhantomJS doesn't support bind yet
@@ -9,256 +9,115 @@ Function.prototype.bind = Function.prototype.bind || function (thisp) {
     };
 };
 
-/*********************
- *  Framework Setup  *
- *********************/
+/******************************
+ *  Base Prototype Formation  *
+ ******************************/
 
-module('Setup');
+module('Exhaustive Module Definition', {
+    descriptorA : {
+        enumerable   : true,
+        configurable : true,
+        writable     : true,
+        value        : 'hello'
+    },
 
-test('Ascot', function() {
-    ok(typeof Ascot === 'function', 'Ascot is a function in the global space');
-});
-
-/***************************
- *  Base Module Formation  *
- ***************************/
-
-module('Base Module', {
-    baseModule : Ascot.defineModule({
-        propA : 'hello',
-        propB : { enm : true, cfg : false, val : 5, wrt: true }
-    }),
-    desc : {
+    descriptorB : {
         enumerable   : true,
         configurable : false,
-        value        : 5,
-        writable     : true
-    }
-});
-
-test('Module creation', function() {
-
-    var mod = this.baseModule();
-
-    ok(mod === Object(mod),
-        'Module was created');
-
-    equal(mod.propA,
-        'hello', 'Normal property was applied');
-
-    deepEqual(
-        Object.getOwnPropertyDescriptor(mod, 'propB'),
-        this.desc,
-        'Descriptor property was applied');
-});
-
-/*****************************
- *  Data/Element/Templating  *
- *****************************/
-
-module('Data/Element/Templating', {
-    data     : { phrase : 'hello world!' },
-    module   : Ascot.defineModule({
-        template : function(data) { return '<div>' + data.phrase + '</div>'; }
-    }),
-
-    setup : function() {
-        document.body.insertAdjacentHTML('beforeend', '<div id="test"></div>');
-        this.module = this.module({
-            data    : this.data,
-            element : document.getElementById('test')
-        });
-        this.module.data = this.data;
+        writable     : true,
+        value        : 5
     },
 
-    teardown : function() {
-        this.module.destroy();
-    }
-});
-
-test('Application', function(){
-
-    deepEqual(this.module.data, this.data,
-        'Data was set on module');
-
-    deepEqual(this.module._element, document.getElementById('test'),
-        'Element was set on module');
-
-    equal(document.getElementById('test').innerHTML, this.data.phrase,
-        'Template applied');
-});
-
-test('Removal', function() {
-    ok(document.getElementById('test'),
-        'Module exists');
-
-    this.module.destroy();
-
-    ok(!document.getElementById('test'),
-        'Module was destroyed');
-});
-
-/********************
- *  Module Options  *
- ********************/
-
-module('Module Options', {
-    options : { optA : true },
-    module : Ascot.defineModule({
-        options : { optA : false }
-    }),
-
     setup : function() {
-        this.module = this.module();
-        this.module.options = this.options;
-        this.module.options = { optB : true };
+        function subtract5(val) { return val - 5; }
+        function returnVal(val) { return val; }
+        function subtract3(val) { return val - 3; }
+        function setPropE(val) { this.propE = val; }
+
+        this.SimpleClass = ascot({
+            propA     : 'hello',
+            propB     : { enm : true, cfg : false, val : 5, wrt: true },
+            funcA     : function() { return 13; },
+            funcB     : function(val) { this.propD = val; },
+            construct : function(val) { this.propC = val; }
+        });
+
+        this.MixedClassA = ascot([this.SimpleClass], {
+            funcA : { $chain   : [this.SimpleClass, subtract5] },
+            funcB : { $iterate : [this.SimpleClass, returnVal] }
+        });
+
+        this.MixedClassB = ascot([this.MixedClassA], {
+            funcA : { $after  : subtract3 },
+            funcB : { $before : setPropE }
+        });
+
+        this.simpleModule = new this.SimpleClass(10);
+        this.mixedModuleA = new this.MixedClassA(11);
+        this.mixedModuleB = new this.MixedClassB(13);
     }
 });
 
-test('Options application', function() {
-    equal(this.module.options.optA, true,
-        'Module options applied');
-    equal(this.module.options.optB, true,
-        'Module options updated');
+test('Descriptor expansion', function() {
+    var SimpleClass = this.SimpleClass;
+
+    deepEqual(SimpleClass.descriptor.propA, this.descriptorA,
+        'A non-configured property was given a default descriptor');
+
+    deepEqual(SimpleClass.descriptor.propB, this.descriptorB,
+        'A shorthand descriptor was expanded appropriately');
 });
 
-/*******************
- *  Data Updating  *
- *******************/
+test('Object construction', function() {
+    var simpleModule = this.simpleModule;
 
-module('Data Updating', {
-    module : Ascot.defineModule({
-        update : function() { this.didUpdate = true; },
-        data : {
-            itemB : {
-                id : 'itemB',
-                itemC : {
-                    id : 'itemC'
-                }
+    deepEqual(Object.getOwnPropertyDescriptor(simpleModule, 'propA'), this.descriptorA,
+        'Default property was defined');
 
-            }
-        }
-    }),
+    deepEqual(Object.getOwnPropertyDescriptor(simpleModule, 'propB'), this.descriptorB,
+        'Shorthand property was defined');
 
-    setup : function() {
-        this.module = this.module();
-        this.module.data = { '#itemC/value' : true };
-        this.module.data = { '#/itemB/value' : true };
-    }
+    equal(simpleModule.propC, 10,
+        'Constructor was called');
 });
 
-test('Data updating', function() {
-    equal(this.module.data.itemB.id, 'itemB',
-        'Additional data added to module data');
-    ok(this.module.data.itemB.itemC.value,
-        'Data updated using an addressed fragment');
-    ok(this.module.data.itemB.value,
-        'Data updated using a root-addressed fragment');
+test('Iterating mixin modifiers', function() {
+    var mixedModuleA = this.mixedModuleA;
+
+    deepEqual(Object.getOwnPropertyDescriptor(mixedModuleA, 'propA'), this.descriptorA,
+        'Default property was defined');
+
+    deepEqual(Object.getOwnPropertyDescriptor(mixedModuleA, 'propB'), this.descriptorB,
+        'Shorthand property was defined');
+
+    equal(mixedModuleA.propC, 11,
+        'Constructor was called');
+
+    equal(mixedModuleA.funcA(), 8,
+        'Chained method called');
+
+    equal(mixedModuleA.funcB(12), 12,
+        'Iterated method returned last value');
+
+    equal(mixedModuleA.propD, 12,
+        'Original method added to iterator and called');
 });
 
-/*********************
- *  Bundle Formation  *
- *********************/
+test('Appending mixin modifiers', function() {
+    var mixedModuleB = this.mixedModuleB;
 
-module('Bundle', {
-    moduleA : Ascot({
-        template : function(data) { return '<div><div id="test">' + data.prompt + '</div><div id="test2"></div></div>'; }
-    }),
-    moduleB : Ascot({
-        template : function() { return '<div>hello, sky!</div>'; }
-    }),
-    moduleC : Ascot({
-        template : function() { return '<div>hello, ocean!</div>'; }
-    }),
-    data : { prompt : 'hello, world!' },
-    test : {},
+    deepEqual(Object.getOwnPropertyDescriptor(mixedModuleB, 'propA'), this.descriptorA,
+        'Default property was defined');
 
-    setup : function() {
-        document.body.insertAdjacentHTML('beforeend', '<div id="main"></div>');
+    deepEqual(Object.getOwnPropertyDescriptor(mixedModuleB, 'propB'), this.descriptorB,
+        'Shorthand property was defined');
 
-        var test = this.test;
+    equal(mixedModuleB.funcA(), 5,
+        'Function appended after chained iterator');
 
-        var main = Ascot.registerBundle('main', {
-            target     : '#main',
-            module     : this.moduleA,
-            controller : function(accessor) { test.accessor = accessor; },
-            settings   : {
-                data : this.data
-            },
+    equal(mixedModuleB.funcB(14), 14,
+        'Iterated method returned last value');
 
-            childA : {
-                target : '#test',
-                bundle : 'moduleB'
-            },
-
-            childB : {
-                settings : {
-                    template : function(data) { return '<div>' + data.prompt + '</div>'; },
-                    data     : this.data
-                },
-                target : '#test2'
-            }
-        });
-
-        Ascot.registerBundle('moduleB', {
-            module : this.moduleB
-        });
-
-        Ascot.registerBundle('moduleC', {
-            module : this.moduleC
-        });
-
-        Ascot.registerBundle('templateA', {
-            target : '#main',
-            settings : {
-                template : function(data) { return '<div>' + data.prompt + '</div>'; },
-                data     : this.data
-            }
-        });
-
-        main.deploy(document);
-    },
-
-    teardown : function() {
-        var test = document.getElementById('main');
-        document.body.removeChild(test);
-    }
-});
-
-test('Bundle formation', function() {
-    ok(document.getElementById('main'),
-        'Test div added to body');
-
-    ok(document.getElementById('test'),
-        'Module template applied');
-
-    equal(document.getElementById('test').innerHTML, 'hello, sky!',
-        'Submodule applied');
-
-    equal(document.getElementById('test2').innerHTML, 'hello, world!',
-        'Template submodule applied');
-
-    equal(document.getElementById('test').id, 'test',
-        'ID copied');
-});
-
-test('Bundle formation with template only', function() {
-    ok(Ascot.bundles.templateA,
-        'Template bundle added to collection');
-
-    Ascot.bundles.templateA.deploy('#main');
-
-    equal(document.getElementById('main').innerHTML, 'hello, world!',
-        'Template applied');
-});
-
-test('Controller calling', function() {
-    ok(typeof this.test.accessor === 'function',
-        'Controller called');
-
-    ok(typeof this.test.accessor.childB === 'function',
-        'Child accessors added to parent accessor');
-
-    ok(this.test.accessor().data,
-        'Accessor returns module');
+    equal(mixedModuleB.propE, 14,
+        'Function appended before iterator');
 });
