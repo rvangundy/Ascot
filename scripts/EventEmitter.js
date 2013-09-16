810db31b9c8869a24baf28cfcf15a3bb18fabeee
rvangundy <ryanvangundy@Todds-MacBook-Pro.local>
2013-09-16 18:56:05 -0400

    Make emit handle multiple arguments

--- scripts/EventEmitter.js
+++ scripts/EventEmitter.js
@@ -43,11 +43,14 @@
      * @param {String}  eventName The name of the event to emit
      * @param {Variant} arg       Any argument to pass to the event listeners
      */
-    function emit(eventName, arg) {
+    function emit(eventName) {
+        var args = Array.prototype.slice.call(arguments, 0);
         var callbacks = this.eventListeners[eventName] = this.eventListeners[eventName] || [];
 
+        args.shift();
+
         for (var i=0, len=callbacks.length; i<len; i+=1) {
-            callbacks[i].call(this, arg);
+            callbacks[i].apply(this, args);
         }
     }
 
