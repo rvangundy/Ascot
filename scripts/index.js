1f2f557660a285d79f7da6b39f65585b13c376d0
rvangundy <ryanvangundy@Todds-MacBook-Pro.local>
2013-09-27 14:59:20 -0400

    Changing to use relative paths

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 'use strict';
-/* jshint unused : false */
 
-var ascot        = require('./Ascot.js');
-var EventEmitter = require('./EventEmitter.js');
-var DOMView      = require('./DOMView.js');
-var Model        = require('./Model.js');
+var ascot = require('./Ascot.js');
+require('./EventEmitter.js');
+require('./DOMView.js');
+require('./Model.js');
 
 module.exports = ascot;
